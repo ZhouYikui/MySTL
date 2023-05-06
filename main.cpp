a8c4d2a3aafbb0440f0f0ba2ee4288cd438dd9d6
ZhouYikui <956386459@qq.com>
2023-05-06 22:01:23 +0800

    update

--- main.cpp
+++ main.cpp
@@ -9,25 +9,9 @@
 
 using namespace std;
 
-void bar(int &&arg)
-{
-    cout << "RHS" << endl;
-}
-
-void bar(const int &arg)
-{
-    cout << "LHS" << endl;
-}
-
-template<typename T>
-void foo(T &&arg)
-{
-    bar(mystl::forward<T>(arg));
-}
-
 int main()
 {
     const int a = 10;
-    foo(10);
+
     return 0;
 }
