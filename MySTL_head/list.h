cf8005ec0650d265aeb304e8c86bd45b7254fd76
ZhouYikui <956386459@qq.com>
2023-05-25 17:08:54 +0800

    update

--- MySTL_head/list.h
+++ MySTL_head/list.h
@@ -537,9 +537,73 @@ namespace mystl
             return fill_insert(pos, n, value);
         }
 
+        template<typename Iter, typename std::enable_if<mystl::is_input_iterator<Iter>::value, int>::type = 0>
+        iterator insert(const_iterator pos, Iter first, Iter last)
+        {
+            size_type n = mystl::distance(first, last);
+            THROW_LENGTH_ERROR_IF(size_ > max_size() - n, "list<T>'s size too big");
+            return copy_insert(pos, n, first);
+        }
+
+        /// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        /// @brief push_front/push_back
+        /// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        void push_front(const value_type &value)
+        {
+            THROW_LENGTH_ERROR_IF(size_ > max_size() - 1, "list<T>'s size too big");
+            auto link_node = create_node(value);
+            link_nodes_at_front(link_node->as_base(), link_node->as_base());
+            ++size_;
+        }
+
+        void push_front(value_type &&value)
+        {
+            emplace_front(mystl::move(value));
+        }
+
+        void push_back(const value_type &value)
+        {
+            THROW_LENGTH_ERROR_IF(size_ > max_size() - 1, "list<T>'s size too big");
+            auto link_node = create_node(value);
+            link_nodes_at_back(link_node->as_base(), link_node->as_base());
+            ++size_;
+        }
+
+        void push_back(value_type &&value)
+        {
+            emplace_back(mystl::move(value));
+        }
+
+        /// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        /// @brief pop_front/pop_back
+        /// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        void pop_front()
+        {
+            MYSTL_DEBUG(!empty());
+            auto n = node_->next;
+            unlink_nodes(n, n);
+            destroy_node(n->as_node());
+            --size_;
+        }
+
+        void pop_back()
+        {
+            MYSTL_DEBUG(!empty());
+            auto n = node_->prev;
+            unlink_nodes(n, n);
+            destroy_node(n->as_node());
+            --size_;
+        }
+
         /// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         /// @brief erase/clear
         /// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        iterator erase(const_iterator pos);
+
+        iterator erase(const_iterator first, const_iterator last);
 
         void clear();
 
@@ -593,6 +657,10 @@ namespace mystl
     /// ================================================================================================================
     /// @brief 容器相关操作
     /// ================================================================================================================
+
+    /// @brief erase
+
+
 
     /// @brief clear, 用于清空链表
 
@@ -768,6 +836,28 @@ namespace mystl
         else
         {
             erase(b, e);
+        }
+    }
+
+    /// @brief copy_assign
+
+    template<typename T>
+    template<typename Iter>
+    void list<T>::copy_assign(Iter f2, Iter l2)
+    {
+        auto f1 = begin();
+        auto l1 = end();
+        for (; f1 != l1 && f2 != l2; ++f1, ++f2)
+        {
+            *f1 = *f2;
+        }
+        if (f2 == l2)
+        {
+            erase(f1, l1);
+        }
+        else
+        {
+            insert(l1, f2, l2);
         }
     }
 
