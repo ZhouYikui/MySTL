1267ea936e3404610271643d6c6383b470608f33
ZhouYikui <956386459@qq.com>
2023-05-26 15:18:21 +0800

    update

---
+++ deque.h
@@ -0,0 +1,18 @@
+//
+// Created by HP on 2023/5/26.
+//
+
+/**
+ * @file deque.h
+ * @brief 实现模板类deque 双端队列
+ *
+ * @date 2023年5月26日
+ * @author ZYK
+ */
+
+#ifndef MYSTL_DEQUE_H
+#define MYSTL_DEQUE_H
+
+
+
+#endif //MYSTL_DEQUE_H
---
+++ queue.h
@@ -0,0 +1,16 @@
+//
+// Created by HP on 2023/5/26.
+//
+
+/**
+ * @file queue.h
+ * @brief 实现模板类queue 和 priority queue
+ *
+ * @date 2023年5月26日
+ * @author ZYK
+ */
+
+#ifndef MYSTL_QUEUE_H
+#define MYSTL_QUEUE_H
+
+#endif //MYSTL_QUEUE_H
